e0f90a39aefa65cdd850bbd1e0d4207d5046affc
Severin Bergsmann <29523118+sbergsmann@users.noreply.github.com>
2025-11-20 11:11:32 +0100

    Add vizualization provider

---
+++ src/benchmarking/services/__init__.py
@@ -0,0 +1,9 @@
+"""
+Barrel export for benchmarking services.
+"""
+
+__all__ = [
+    "PredictionVizualizationProvider",
+]
+
+from .prediction_vizualization_provider import PredictionVizualizationProvider
