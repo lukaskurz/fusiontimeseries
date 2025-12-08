3a5b804397ff0d3c996513df98eca2f5fd38de6a
Severin Bergsmann <29523118+sbergsmann@users.noreply.github.com>
2025-12-08 11:19:07 +0100

    Remove deprecated pipelines

--- src/benchmarking/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Barrel export for benchmarking services.
-"""
-
-__all__ = [
-    "PredictionVizualizationProvider",
-]
-
-from .prediction_vizualization_provider import PredictionVizualizationProvider
